ebfe1e191ec11bf0bef8ee8e1fedce3da2149b73
Kazuki Oikawa <k@oikw.org>
2010-03-03 09:50:04 +0000

    アセンブリ情報を修正

--- TwitterStreaming/Properties/AssemblyInfo.cs
+++ TwitterStreaming/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle ("TwitterStreaming")]
-[assembly: AssemblyDescription ("")]
+[assembly: AssemblyTitle ("ktwt")]
+[assembly: AssemblyDescription ("Simple Twitter Client with OAuth-REST/Streaming API")]
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
-[assembly: AssemblyProduct ("TwitterStreaming")]
+[assembly: AssemblyProduct ("")]
 [assembly: AssemblyCopyright ("Copyright © Kazuki Oikawa 2010")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion ("0.0.4.0")]
+[assembly: AssemblyVersion ("0.0.4.*")]
 //[assembly: AssemblyFileVersion ("0.0.")]
